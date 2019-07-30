2a7e8ccc0922ff63b7df1d6b0ba55a18cf32b3f3
ananda.rifkiy32 <ananda.rifkiy@gmail.com>
2019-07-30 21:44:27 +0700

    add login function

--- application/controllers/V1.php
+++ application/controllers/V1.php
@@ -46,10 +46,8 @@ class V1 extends CI_Controller {
 		$tokenData['password'] = $data['password'];
 
 		if ($this->Usermodel->register($data)){
-			$token = AUTHORIZATION::generateToken($tokenData);
 			$response =[
 				'status' => 200,
-				'token' => $token,
 				'msg' => 'Request Successful !'
 			];
 			$this->response($response, 200);
@@ -58,13 +56,65 @@ class V1 extends CI_Controller {
 				'status' => 401,
 				'msg'	=> 'Request Failed !'
 			];
-			$this->response(["token" => "000"], 401);
+			$this->response($response, 401);
 		}
 	}
 
 	private function login_post(){
-		$data = $this->verify_request();
-		$this->response($data, 200);
+		$config = [
+			[
+					'field' => 'email',
+					'label' => 'Email',
+					'rules' => 'required|max_length[50]|valid_email',
+					'errors' => [
+							'required' => 'We need both username and password',
+							'max_length' => 'Maximum Email length is 50 characters',
+							'valid_email' => 'Email not valid',
+					],
+			],
+			[
+					'field' => 'password',
+					'label' => 'Password',
+					'rules' => 'required',
+					'errors' => [
+							'required' => 'You must provide a Password.',
+					],
+			],
+		];
+		
+		$data = $this->input->post();
+		
+		$this->form_validation->set_data($data);
+		$this->form_validation->set_rules($config);
+
+		if($this->form_validation->run()==FALSE){
+			$this->response($this->form_validation->error_array(), 400);
+			exit();
+		}
+
+
+		///
+		$data['email'] = $this->post('email');
+		$data['password'] = $this->post('password');
+
+		if ($this->Usermodel->select_user($data)->num_rows() == 1){
+			$token = AUTHORIZATION::generateToken($data);
+			$response = array(
+				'status' => 200,
+				'message' => 'Login Successful !',
+				'token' => $token
+			);
+
+			$this->response($response, 200);
+		} else {
+			$response = array(
+				'status' => 400,
+				'message' => 'Email or Password did not match !'
+			);
+			$this->response($response, 400);
+
+		}
+
 	}
 	
 	private function verify_request()
