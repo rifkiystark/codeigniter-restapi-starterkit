50509fdb98ef7c7e1a894fc50aa0d526549a8c7d
ananda.rifkiy32 <ananda.rifkiy@gmail.com>
2020-02-05 13:24:43 +0700

    init

--- application/models/Tempordermodel.php
+++ application/models/Tempordermodel.php
@@ -3,8 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Tempordermodel extends CI_Model {
 
-    function select($data){
+    function select(){
         return $this->db->get('temporders');
+    }
+
+    function select_where($where){
+        return $this->db->get_where('temporders', $where);
     }
 
     function insert($data){
--- identities/index.php
+++
@@ -1,1 +0,0 @@
-Anda dilarang masuk
