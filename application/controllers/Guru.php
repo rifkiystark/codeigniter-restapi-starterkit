f37ce10e0fcb2b4b470390fe1028a04a801c44e6
ananda.rifkiy32 <ananda.rifkiy@gmail.com>
2020-02-26 20:01:37 +0700

    add guru controller

---
+++ application/controllers/Guru.php
@@ -0,0 +1,97 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+use Restserver\Libraries\REST_Controller;
+
+require APPPATH . '/libraries/REST_Controller.php';
+require APPPATH . '/libraries/Format.php';
+// require APPPATH . '/helpers/authorization_helper.php';
+// require APPPATH . '/helpers/Validator.php';
+// First, run 'composer require pusher/pusher-php-server'
+
+require '././vendor/autoload.php';
+
+
+
+class Guru extends CI_Controller
+{
+
+	use REST_Controller {
+		REST_Controller::__construct as private __resTraitConstruct;
+	}
+
+
+	function __construct()
+	{
+		// Construct the parent class
+		parent::__construct();
+		$this->__resTraitConstruct();
+		$this->load->model(array('GuruModel', 'MasterSiswaModel'));
+		$this->load->helper(['jwt', 'authorization', 'Validator']);
+	}
+
+	function index()
+	{
+		echo "wakwaw";
+	}
+
+	function upload_post()
+	{
+		header("Access-Control-Allow-Origin: *");
+		$this->response(["Status" => 200], 200);
+	}
+
+	function login_post()
+	{
+		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Expose-Headers: Content-Length, X-JSON");
+		header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+		header("Access-Control-Allow-Headers: *");
+
+		$this->form_validation->set_data($this->post());
+		$this->form_validation->set_rules(Validator::Login());
+
+		if ($this->form_validation->run() == FALSE) {
+			$data = [
+				'status' => 'Failed',
+				'msg' => $this->validation_errors()
+			];
+			$this->response($data, 400);
+			exit();
+		}
+		$data['email'] = $this->post('email');
+		$data['password'] = md5($this->post('password'));
+
+		$dataLogin = $this->GuruModel->get_where($data);
+		$dataResponse['wali'] = [
+			'email' => $dataLogin->row('email'),
+			'nama' => $dataLogin->row('nama'),
+			'jenkel' => $dataLogin->row('jenkel')
+		];
+		$dataResponse['siswa'] = [];
+		$dataToken['wali'] = $dataLogin->row('id_wali');
+		$dataToken['siswa'] = [];
+		if ($dataLogin->num_rows() > 0) {
+			$where = [
+				'id_wali' => $dataLogin->row('id_wali')
+			];
+			$dataSiswa = $this->MasterSiswaModel->get_where($where)->result();
+			foreach ($dataSiswa as $siswa) {
+				array_push($dataToken['siswa'], $siswa->id_master_siswa);
+				unset($siswa->id_master_siswa);
+				unset($siswa->id_wali);
+				array_push($dataResponse['siswa'], $siswa);
+			}
+
+			$token = AUTHORIZATION::generateToken($dataToken);
+			$dataResponse['token'] = $token;
+			$this->response($dataResponse, 200);
+		} else {
+			$response = [
+				'status' => 'Failed',
+				'msg' => 'Email atau Password salah'
+			];
+			$this->response($response, 400);
+		}
+	}
+}
---
+++ application/models/GuruModel.php
@@ -0,0 +1,25 @@
+<?php
+class GuruModel extends CI_Model
+{
+    function __construct()
+    {
+        $this->table = 'Guru';
+    }
+
+
+    function get()
+    {
+        return $this->db->get($this->table);
+    }
+
+    function insert($data)
+    {
+        return $this->db->insert($this->table, $data);
+    }
+
+    function get_where($where)
+    {
+        $this->db->where($where);
+        return $this->db->get($this->table);
+    }
+}
